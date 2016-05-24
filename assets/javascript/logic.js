8f5c8617d92aad7081cb73a29088e40b5491ade1
Kenny Victoria <kvict3@gmail.com>
2016-05-24 11:52:42 -0400

    Delete logic.js

--- assets/javascript/logic.js
+++
@@ -1,5 +0,0 @@
-//link to firebase
-
-
-
-var teams = ["Buffalo Bills", "Miami Dolphins", "New England Patriots", "New York Jets", "Baltimore Ravens", "Cincinatti Bengals", "Cleveland Browns", "Pittsburg Steelers", "Houstan Texans", "Indianapolis Colts", "Jacksonville Jaguars", "Tennessee Titans", "Denver Broncos", "Kansas City Chiefs", "Oakland Raiders", "San Diego Chargers", "Dallas Cowboys", "New York Giants", "Philadelphia Eagles", "Washington Redskins", "Chicago Bears", "Detroit Lions", "Green Bay Packers", "Minnesota Vikins", "Atlanta Falcons", "Carolina Panthers", "New Orleans Saints", "Tampa Bay Buccaneers", "Arizona Cardinals", "Los Angelas Rams", "San Francisco 49ers", "Seattle Seahawks"];
