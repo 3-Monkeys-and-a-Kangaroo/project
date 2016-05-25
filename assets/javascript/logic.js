cd378f0519afaabd23931ce894df837930ea6011
Morgan McGee <morgan@fanexperiences.com>
2016-05-25 18:49:36 -0400

    initial upload

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+// <<<<<<< HEAD
 // SETUP VARIABLES
 // =========================================
 var authKey = "9d4a8986921972b65754ea0809d47c84:12:74623931";
@@ -115,10 +115,8 @@ $('#searchBtn').on('click', function() {
 	return false;
 
 })
-=======
+// =======
 //link to firebase
 
 
 
-var teams = ["Buffalo Bills", "Miami Dolphins", "New England Patriots", "New York Jets", "Baltimore Ravens", "Cincinatti Bengals", "Cleveland Browns", "Pittsburg Steelers", "Houstan Texans", "Indianapolis Colts", "Jacksonville Jaguars", "Tennessee Titans", "Denver Broncos", "Kansas City Chiefs", "Oakland Raiders", "San Diego Chargers", "Dallas Cowboys", "New York Giants", "Philadelphia Eagles", "Washington Redskins", "Chicago Bears", "Detroit Lions", "Green Bay Packers", "Minnesota Vikins", "Atlanta Falcons", "Carolina Panthers", "New Orleans Saints", "Tampa Bay Buccaneers", "Arizona Cardinals", "Los Angelas Rams", "San Francisco 49ers", "Seattle Seahawks"];
->>>>>>> eadbe6ca788b7daae40dae3fc85bb65de7d66f18
